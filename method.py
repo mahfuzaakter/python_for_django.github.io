4e2bab68cf6e961c9915498a9b023886c0146977
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-25 19:41:16 +0600

    Add files via upload

---
+++ method.py
@@ -0,0 +1,10 @@
+mylist = ["bangladish","india","usa","france","china","uk","canada"]
+
+mylist.append("germany")
+mylist.append(["pakistan","japan"])
+mylist.insert(2,"sudia arabia")
+mylist.remove("france")
+mylist.pop()
+country = mylist.pop()
+print(mylist)
+print(country)
