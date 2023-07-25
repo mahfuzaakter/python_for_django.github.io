0c4c2c24a309831e11d4f90dd9841f945449fec7
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-25 19:27:26 +0600

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,9 @@
+mylist = ["bangladish","india","usa","france","china","uk","canada"]
+newlist = mylist[2:6]  #start and end slice
+
+
+list= mylist[::2]  # step slice
+nlist =mylist[-1::-2]
+print(newlist)
+print(list)
+print(nlist)
