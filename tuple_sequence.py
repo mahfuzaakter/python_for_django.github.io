c8dc51e662b69512b85592605d144d24a205cf08
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-25 23:33:34 +0600

    Add files via upload

---
+++ tuple_sequence.py
@@ -0,0 +1,11 @@
+tuples =( "user", 20, "dhaka")
+
+print(tuples)
+tuples = list(tuples)  #list e convert
+tuples.append(["address","number"])  #item add
+print(tuples)
+print(type(tuples))
+
+tuples=tuple(tuples) #tuple e convert
+print(tuples)
+print(type(tuples))
