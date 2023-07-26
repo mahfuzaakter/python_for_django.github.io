135e2235903aaa45479d8a1bddd6edf1b9197d45
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-27 00:27:35 +0600

    Add files via upload

---
+++ range_in_loop.py
@@ -0,0 +1,10 @@
+mylist =list(range(1,10)) #range function diye list iterate
+print(mylist)
+
+for i in range(1,10):   #range function diye loop iterate
+    print(i)
+
+mylist1 = ["english","bangla","hindi","urdu"]
+
+for i in range(len(mylist1)):
+    print(f"language: {mylist1[i]}")
