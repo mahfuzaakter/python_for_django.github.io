83de8a146c0b23784a826b3fa422c56e23f7ada4
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-26 14:00:00 +0600

    Add files via upload

---
+++ set.py
@@ -0,0 +1,23 @@
+myset = {"user",20}
+print(myset)
+
+myset = set()# empty set
+print(myset)
+
+set1 = set ("abcdefl")
+print(set1)
+
+set2 = set ("efghibkl")
+print(set2)
+
+#operation
+print(set1 & set2)#intersection
+
+print(set1 | set2)#union
+
+print(set1 - set2)
+
+print(set2 - set1)
+
+print(set1^set2)
+
