c6faa0c75528750eb290b569587aa8555b30db88
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-26 21:56:35 +0600

    Add files via upload

---
+++ common_sequence_operation.py
@@ -0,0 +1,15 @@
+mytuple = ("user","address",23)
+#concatinate er jonno
+mytuple2 =("name")
+# mytuple + mytuple2
+mylist =["user","address",23]
+
+myrange = range(0,51)
+print("user"in mytuple)
+print(100 in myrange)
+print("name"in mylist)
+print(100 not in myrange)
+print(mylist.len())
+print(mytuple[0])
+
+mylist.count()
