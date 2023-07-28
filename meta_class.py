7407953d6aa6b5a57417a086f7c3ea693abd6654
mahfuzaakter <83703776+mahfuzaakter@users.noreply.github.com>
2023-07-29 00:21:23 +0600

    Add files via upload

---
+++ meta_class.py
@@ -0,0 +1,21 @@
+#Meta class=> class of class
+# evething is object
+
+mystr ="bohubrihi"
+mynum = 23
+mydict ={"name": "user"}
+mylist =[1,2,3,4,5]
+
+class A:
+    pass
+
+obj =A()
+
+print(type(obj))
+print(type(A))
+
+
+print(type(mystr))
+print(type(mydict))
+print(type(mylist))
+print(type(mynum))
